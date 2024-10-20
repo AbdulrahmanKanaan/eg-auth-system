b6e00164b13152aa5d34ff32bc794ea66dbc7ff5
AbdulrahmanKanaan <abdulrahman.kanaan44@gmail.com>
2024-10-20 20:56:52 +0400

    remove temp code

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,6 @@ import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import bootstraps from './bootstrap';
-import { UsersService } from './users';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,14 +17,4 @@ bootstrap().then(afterBootstrap).catch(console.log);
 
 async function afterBootstrap(app: INestApplication) {
   console.log(`Server running on ${await app.getUrl()}`);
-
-  const usersService = app.get(UsersService);
-
-  const newUser = await usersService.create({
-    email: 'qwe',
-    name: 'qwe',
-    password: 'qwe',
-  });
-
-  console.log(newUser);
 }
